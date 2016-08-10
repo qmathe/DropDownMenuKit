ac1927e141545ed98c5e1b69fa13989b7fe0a3a7
Quentin Mathé <quentin.mathe@gmail.com>
2016-08-10 17:04:39 +0200

    Reorganized into framework and example app

---
+++ DropDownMenu.h
@@ -0,0 +1,18 @@
+/*
+	Copyright (C) 2016 Quentin Mathe
+ 
+	Date:  August 2016
+	License:  MIT
+ */
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DropDownMenu.
+FOUNDATION_EXPORT double DropDownMenuVersionNumber;
+
+//! Project version string for DropDownMenu.
+FOUNDATION_EXPORT const unsigned char DropDownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropDownMenu/PublicHeader.h>
+
