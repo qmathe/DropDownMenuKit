080ff31fa937f3aff65028fd3d6606d9036604f8
Quentin Mathé <quentin.mathe@gmail.com>
2016-08-11 09:49:59 +0200

    Wrote example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,25 +1,164 @@
-//
-//  ViewController.swift
-//  DropDownMenu
-//
-//  Created by Quentin Mathé on 02/08/2016.
-//  Copyright © 2016 Quentin Mathé. All rights reserved.
-//
+/*
+	Copyright (C) 2016 Quentin Mathe
+ 
+	Date:  August 2016
+	License:  MIT
+ */
 
 import UIKit
+import DropDownMenu
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, DropDownMenuDelegate {
+
+	var titleView: DropDownTitleView!
+	@IBOutlet var navigationBarMenu: DropDownMenu!
+	@IBOutlet var toolbarMenu: DropDownMenu!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		let title = prepareNavigationBarMenuTitleView()
+
+		prepareNavigationBarMenu(title)
+		prepareToolbarMenu()
+	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+
+		navigationBarMenu.container = view
+		toolbarMenu.container = view
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func prepareNavigationBarMenuTitleView() -> String {
+		// Both title label and image view are fixed horizontally inside title
+		// view, UIKit is responsible to center title view in the navigation bar.
+		// We want to ensure the space between title and image remains constant, 
+		// even when title view is moved to remain centered (but never resized).
+		titleView = DropDownTitleView(frame: CGRect(x: 0, y: 0, width: 150, height: 40))
+		titleView.addTarget(self,
+		            action: #selector(ViewController.willToggleMenu(_:)),
+		          forControlEvents: .TouchUpInside)
+		titleView.addTarget(self,
+		                    action: #selector(ViewController.didToggleMenu(_:)),
+		          forControlEvents: .ValueChanged)
+		titleView.titleLabel.textColor = UIColor.blackColor()
+		titleView.title = "Large"
+
+		navigationItem.titleView = titleView
+		
+		return titleView.title!
+	}
+	
+	func prepareNavigationBarMenu(currentChoice: String) {
+		navigationBarMenu = DropDownMenu(frame: view.bounds)
+		navigationBarMenu.delegate = self
+		
+		let firstCell = DropDownMenuCell()
+		
+		firstCell.textLabel!.text = "Large"
+		firstCell.menuAction = #selector(ViewController.choose(_:))
+		firstCell.menuTarget = self
+		if currentChoice == "Large" {
+			firstCell.accessoryType = .Checkmark
+		}
+		
+		let secondCell = DropDownMenuCell()
+		
+		secondCell.textLabel!.text = "Small"
+		secondCell.menuAction = #selector(ViewController.choose(_:))
+		secondCell.menuTarget = self
+		if currentChoice == "Small" {
+			firstCell.accessoryType = .Checkmark
+		}
+
+		navigationBarMenu.menuCells = [firstCell, secondCell]
+		
+		// If we set the container to the controller view, the value must be set
+		// on the hidden content offset (not the visible one)
+		navigationBarMenu.visibleContentOffset =
+			navigationController!.navigationBar.frame.size.height + statusBarHeight()
+
+		// For a simple gray overlay in background
+		navigationBarMenu.backgroundView = UIView(frame: navigationBarMenu.bounds)
+		navigationBarMenu.backgroundView!.backgroundColor = UIColor.blackColor()
+		navigationBarMenu.backgroundAlpha = 0.7
 	}
 
+	func prepareToolbarMenu() {
 
+		toolbarMenu = DropDownMenu(frame: view.bounds)
+		toolbarMenu.delegate = self
+		
+		let selectCell = DropDownMenuCell()
+		
+		selectCell.textLabel!.text = "Select"
+		selectCell.imageView!.image = UIImage(named: "Ionicons-ios-checkmark-outline")
+		selectCell.showsCheckmark = false
+		selectCell.menuAction = #selector(ViewController.select as (ViewController) -> () -> ())
+		selectCell.menuTarget = self
+		
+		let sortKeys = ["Name", "Date", "Size"]
+		let sortCell = DropDownMenuCell()
+		let sortSwitcher = UISegmentedControl(items: sortKeys)
+
+		sortSwitcher.selectedSegmentIndex = sortKeys.indexOf("Name")!
+		sortSwitcher.addTarget(self, action: #selector(ViewController.sort(_:)), forControlEvents: .ValueChanged)
+
+		sortCell.customView = sortSwitcher
+		sortCell.textLabel!.text = "Sort"
+		sortCell.imageView!.image = UIImage(named: "Ionicons-ios-search")
+		sortCell.showsCheckmark = false
+
+		toolbarMenu.menuCells = [selectCell, sortCell]
+
+		// If we set the container to the controller view, the value must be set
+		// on the hidden content offset (not the visible one)
+		toolbarMenu.visibleContentOffset =
+			navigationController!.navigationBar.frame.size.height + statusBarHeight()
+
+		// For a simple gray overlay in background
+		toolbarMenu.backgroundView = UIView(frame: toolbarMenu.bounds)
+		toolbarMenu.backgroundView!.backgroundColor = UIColor.blackColor()
+		toolbarMenu.backgroundAlpha = 0.7
+	}
+
+	@IBAction func choose(sender: AnyObject) {
+		titleView.title = (sender as! DropDownMenuCell).textLabel!.text
+	}
+
+	@IBAction func select() {
+		print("Sent select action")
+	}
+
+	@IBAction func sort(sender: AnyObject) {
+		print("Sent sort action")
+	}
+
+	@IBAction func showToolbarMenu() {
+		toolbarMenu.show()
+	}
+	
+	@IBAction func willToggleMenu(sender: DropDownTitleView) {
+		if sender.isUp {
+			navigationBarMenu.hide()
+		}
+		else {
+			navigationBarMenu.show()
+		}
+	}
+
+	@IBAction func didToggleMenu(sender: DropDownTitleView) {
+		print("Sent did toogle menu action")
+	}
+
+	func didTapInDropDownMenuBackground(menu: DropDownMenu) {
+		menu.hide()
+	}
 }
 
+
+func statusBarHeight() -> CGFloat {
+	let statusBarSize = UIApplication.sharedApplication().statusBarFrame.size
+	return min(statusBarSize.width, statusBarSize.height)
+}
