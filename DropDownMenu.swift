45ef9843ce56fa0e809777dd1ada254014ee8403
Herrick Wolber <wolbereric@yahoo.fr>
2017-09-13 16:33:11 +0200

    Few indentations fixes

--- DropDownMenu.swift
+++ DropDownMenu.swift
@@ -79,7 +79,8 @@ open class DropDownMenu : UIView, UITableViewDataSource, UITableViewDelegate, UI
 		}
 	}
 	open var backgroundAlpha = CGFloat(1)
-	
+	var newTableViewFrame : CGRect
+    
 	// MARK: - Initialization
 	
 	override public init(frame: CGRect) {
@@ -89,6 +90,12 @@ open class DropDownMenu : UIView, UITableViewDataSource, UITableViewDelegate, UI
 		menuView = UITableView(frame: CGRect(origin: CGPoint.zero, size: frame.size))
 		menuView.autoresizingMask = .flexibleWidth
 		menuView.isScrollEnabled = false
+        menuView.isScrollEnabled = true
+        menuView.bounces = false
+        menuView.showsVerticalScrollIndicator = false
+        menuView.showsHorizontalScrollIndicator = false
+        
+        newTableViewFrame = frame
 
 		contentView.addSubview(menuView)
 
@@ -115,9 +122,23 @@ open class DropDownMenu : UIView, UITableViewDataSource, UITableViewDelegate, UI
 	
 	open override func layoutSubviews() {
 		super.layoutSubviews()
+        
         let contentHeight = menuCells.reduce(0) { $0 + $1.rowHeight }
-		
+        
 		menuView.frame.size.height = contentHeight
+        
+        //  Set the right height for tableView
+        //
+        //  If the total from the cells is less thant the height of the screen
+        //  then we keep all cells height
+        //  Else we take keep the the height of the screen so we can scroll from all its elements
+        
+        if contentHeight <= newTableViewFrame.size.height {
+            menuView.frame.size.height = contentHeight
+        } else {
+            menuView.frame.size.height = newTableViewFrame.size.height - visibleContentOffset
+        }
+        
 		contentView.frame.size.height = menuView.frame.size.height
 	}
 	
